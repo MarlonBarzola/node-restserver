0d74a5b1d1e6f36e58ffdb4b50e1cf1858478bb8
Marlon <marlonbarzola@hotmail.com>
2020-01-13 11:42:34 -0500

    Pagina de inicio del servidor

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+    res.json({
+        ok: true,
+        message: 'Index Page'
+    });
+});
+
 app.use(require('./usuario'));
 app.use(require('./login'));
 
