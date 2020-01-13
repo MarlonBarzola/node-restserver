c634b7d8aff48535ae5c6ae8aef36c332aa8dc3b
Marlon <marlonbarzola@hotmail.com>
2020-01-13 11:49:27 -0500

    Login normal sección 10

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.json({
-        ok: true,
-        message: 'Index Page'
-    });
-});
-
 app.use(require('./usuario'));
 app.use(require('./login'));
 
